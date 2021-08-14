ca3378491b328b76c6263c4e0576af7832f4476a
BilginistAdam <bilginenesismail@gmail.com>
2021-08-14 14:29:34 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mcuDevelopmentBaremetal
+This repository includes the some MCU (E.g., STM32F103, STM32F411, etc.) bare-metal template files.
---
+++ STM32/STM32F411CEUx/Src/main.c
@@ -0,0 +1,21 @@
+/**
+ * @file main.c
+ * @author Ismail Enes Bilgin - bilginist (bilginenesismail@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-08-12
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "main.h"
+
+int main(void){
+
+    while (1){
+
+    }
+
+    return 0;
+}
