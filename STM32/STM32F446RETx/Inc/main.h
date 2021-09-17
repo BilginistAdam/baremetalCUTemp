0b398d8e2ba1b3dc26db929ec01857f1ddf0dab3
Ismail Enes Bilgin <ienesbilgin@bilginier.com>
2021-09-18 01:13:15 +0300

    Added STM32F446RE Device Temp.

---
+++ STM32/STM32F446RETx/Inc/main.h
@@ -0,0 +1,15 @@
+/**
+ * @file main.h
+ * @author Ismail Enes Bilgin - bilginist (bilginenesismail@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-08-12
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __MAIN_H_
+#define __MAIN_H_
+
+#endif /* __MAIN_H_ */
---
+++ STM32/STM32F446RETx/readme.md
@@ -0,0 +1,1 @@
+This folder includes the temp files of the STM32F411CE bare metal development materials.
